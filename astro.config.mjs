34d2b8ba3713c82f1c46dfa759792a531b93fa5c
Playmaker3334 <jikjfeippk123@gmail.com>
2025-03-27 15:04:49 -0600

    Add .nojekyll file and update GitHub workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://playmaker3334.github.io',
   base: '/EA_website',
-  // otras configuraciones que ya tengas
 });
