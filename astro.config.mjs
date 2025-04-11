89457151f1031f130bd38a0c6e81d6ca73a91143
Jason Pinelo <jpinelo@covalto.com>
2025-04-11 05:39:55 -0500

    Revert to working configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,4 @@ export default defineConfig({
     host: '0.0.0.0',
   },
   site: 'https://even-more-effective.github.io',
-  trailingSlash: 'always',
-  build: {
-    format: 'directory'
-  },
 });
